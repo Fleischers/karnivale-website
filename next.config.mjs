d1922813fe010e484c2da395d6440f9e762756d8
Fleischers <Fleischer458@gmail.com>
2026-01-28 10:51:50 +0200

    prepare for Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,13 @@ const withMDX = mdx({
   options: {},
 });
 
+const repoName = '/karnivale-website';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: repoName,
+  assetPrefix: repoName,
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
